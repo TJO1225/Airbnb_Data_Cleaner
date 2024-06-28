84c9b0387c637c379280b3f1eb372284f1b9fece
tjo12 <tjo1225@gmail.com>
2024-06-28 02:49:22 +0000

    Updates for deployment

--- app/config.py
+++ app/config.py
@@ -1,9 +1,20 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "postgresql://sqlAdmin:Airbnbdata1@sqldb-asdk4-strag4-w8qh.database.windows.net:1433/sqldb-tenant-asdk4-strag4-w8qh"
-    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
     TEMP_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'temp')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    DEBUG = os.getenv('FLASK_DEBUG', 'False') == 'True'
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://sqlAdmin:Airbnbdata1@sqldb-asdk4-strag4-w8qh.database.windows.net:1433/sqldb-tenant-asdk4-strag4-w8qh')
+    DEBUG = True
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    DEBUG = False
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+}
